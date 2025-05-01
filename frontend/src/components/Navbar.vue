8d5f21f5ce3728897b300005077760b229acf850
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-01 12:20:02 -0300

    criação basica da navegação

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/" >Início</router-link></li>
+      <li><router-link to="/products" >Produtos</router-link></li>
+      <li><router-link to="/productform">Novo Produto</router-link></li>
+    </ul>
+  </nav>
+</template>
+  
+  
