5db6ef13bb61c2d584e6d0b0721abe0598d85367
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-02 11:50:08 -0300

    conexão com o banco de dados

---
+++ backend/database.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const uri = process.env.MONGO_URI as string
+
+export const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(uri)
+        console.log('Banco de dados conectado')
+    }catch(err){
+        console.error('Erro ao conectar no banco de dados', err)
+        process.exit(1)
+    }
+}
