acae0fa4606ce39f51a583940869c3a897b49a8e
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-02 19:10:16 -0300

    criação da rota /api e a utilização da mesma para a criação de categorias

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { connectToDatabase } from './database'
+import categoryRoutes from './src/routers/categoryRoutes'
 
 const app = express()
 app.use(express.json())
 
 connectToDatabase()
+
+app.use('/api', categoryRoutes)
 
 app.listen(3000, () => {
     console.log('Api rodando...')
