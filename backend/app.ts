bb6a272d5e363fef4d44501c725f088a9a96d724
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-03 10:32:23 -0300

    criação da rota para a criação do produto

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connectToDatabase } from './database'
 import categoryRoutes from './src/routers/categoryRouter'
+import productRouter from './src/routers/productRouter'
 
 const app = express()
 app.use(express.json())
@@ -8,6 +9,7 @@ app.use(express.json())
 connectToDatabase()
 
 app.use('/api', categoryRoutes)
+app.use('/api', productRouter)
 
 app.listen(3000, () => {
     console.log('Api rodando...')
