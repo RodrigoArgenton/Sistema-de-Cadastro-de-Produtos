05bdebfb794754a9a30cdaea7e4ad8075bca10ed
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-03 15:36:32 -0300

    criação da rota para a busca de categorias

--- backend/src/routers/categoryRouter.ts
+++ backend/src/routers/categoryRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { handleCreateCategory } from "../controllers/categoryController"
+import { getCategories } from '../controllers/categoryController'
 
 const router = Router()
 
 router.post('/categories', handleCreateCategory)
+router.get('/categories', getCategories)
 
 export default router
