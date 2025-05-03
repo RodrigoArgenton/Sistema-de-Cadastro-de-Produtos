de7d095fe0dee2cb48024763aab3e04eb56c8d46
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-03 10:33:32 -0300

    edição no endereço

--- backend/src/services/categoryService.ts
+++ backend/src/services/categoryService.ts
@@ -1,4 +1,4 @@
-import { Category } from "../models/CategoryModel"
+import { Category } from "../models/categoryModel"
 
 export const createCategory = async (name: string, parentId?: string | null) => {
     const category = new Category({ name, parentId: parentId || null})
