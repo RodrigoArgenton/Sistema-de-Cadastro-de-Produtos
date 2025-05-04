a4d71355f88d98e6b92f39b90122b75f794076b7
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-04 19:27:19 -0300

    rota get para produtos

--- backend/src/routers/productRouter.ts
+++ backend/src/routers/productRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { handleCreateProduct } from "../controllers/productController"
+import { getProductsController } from "../controllers/productController"
 
 const router = Router()
 
 router.post('/products', handleCreateProduct)
+router.get('/products', getProductsController)
 
 export default router
