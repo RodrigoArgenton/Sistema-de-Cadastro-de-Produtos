6d4a9052459c1c1418257713e77292b678ed7101
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2025-05-04 18:26:19 -0300

    inclusão do cors

--- backend/app.ts
+++ backend/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import { connectToDatabase } from './database'
 import categoryRoutes from './src/routers/categoryRouter'
 import productRouter from './src/routers/productRouter'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 connectToDatabase()
